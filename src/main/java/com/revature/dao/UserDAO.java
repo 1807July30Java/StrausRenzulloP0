25df09a77cc5c07c551614f52b0d8b87f98ec790
Jeremy Straus <Jstraws@verizon.net>
2018-08-13 13:46:00 -0400

    Whoops

--- src/main/java/com/revature/dao/UserDAO.java
+++ src/main/java/com/revature/dao/UserDAO.java
@@ -3,4 +3,6 @@ import com.revature.pojo.User;
 public interface UserDAO{
     public User getUser(String username, String pass);
     public void newUser(String username,String password,boolean isAdmin);
+    public boolean elevateUser(String username);
+    public boolean deleteUser(String username);
 }
